7606a964dc7168037cd7852538be8ebc98164080
shirazJafri <shirazJafri@outlook.com>
2023-09-02 21:34:41 +0500

    Fix health routes

--- ruby/app/routes/health.rb
+++ ruby/app/routes/health.rb
@@ -1,8 +1,9 @@
 class HealthRoutes < Sinatra::Base
     
   get('/') do
-    if request.env['AUTHED'] == true
-      'App working OK'
+    if request.env['HTTP_AUTHORIZATION'].present?
+      status 200
+      body 'App working OK'
     else
       status 403
     end
